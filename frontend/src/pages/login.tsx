335e950db42519459829f27cb2fdc7b22b33c428
Lucas Sena <lucassena.rj@gmail.com>
2025-07-23 14:19:45 +0100

    fix(frontend): fix sign in button

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -61,7 +61,12 @@ function LoginPage() {
                     {...register("username", { required: "Username is required" })}
                   />
                 </div>
-                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <button
+                  type="submit"
+                  className="w-full text-white text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+                >
+                  Sign in
+                </button>
               </form>
             </div>
           </div>
